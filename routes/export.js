036d8b3104a6e290cdbc6196926c60763a1dc949
davide <davide.molinelli@mail.polimi.it>
2023-11-11 12:19:37 +0100

    Users authentication + script for repository populate

--- routes/export.js
+++ routes/export.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const JSZip = require('jszip');
+const {getUser} = require("./middlewares/users");
 const Repository = require('../models/Repository').Repository;
 const RepositoryClass = require("../models/RepositoryClass").RepositoryClass;
 const JDoctorCondition = require("../models/JDoctorCondition").JDoctorCondition;
@@ -13,6 +14,82 @@ router.get(
         try {
             const zip = new JSZip();
             const repositories = await Repository.find();
+            const folderStructure = {
+                "inputProjects.json": JSON.stringify(repositories.map(r => { return {
+                    projectName: r.projectName,
+                    rootPathList: r.rootPathList,
+                    srcPathList: ["raw", ...r.srcPathList],
+                    jarPathList: ["jar"],
+                    jDoctorConditionsPathList: ["conditions"]
+                } }), null, 4),
+            };
+
+            for (const repository of repositories) {
+                for (const idRepositoryClass of repository.classes) {
+                    const repositoryClass = await RepositoryClass.findById(idRepositoryClass);
+                    const jDoctorConditions = [];
+
+                    for (const idJDoctorCondition of repositoryClass.jDoctorConditions) {
+                        const jDoctorCondition = await JDoctorCondition.findById(idJDoctorCondition);
+                        const preConditions = [];
+                        for (const idPreCondition of jDoctorCondition.pre) {
+                            const preCondition = await PreCondition.findById(idPreCondition);
+                            preConditions.push(preCondition);
+                        }
+                        const postConditions = [];
+                        for (const idPostCondition of jDoctorCondition.post) {
+                            const postCondition = await PostCondition.findById(idPostCondition);
+                            postConditions.push(postCondition);
+                        }
+                        const throwsConditions = [];
+                        for (const idThrowsCondition of jDoctorCondition.throws) {
+                            const throwsCondition = await ThrowsCondition.findById(idThrowsCondition);
+                            throwsConditions.push(throwsCondition);
+                        }
+
+                        jDoctorConditions.push({
+                            source: jDoctorCondition.source,
+                            operation: jDoctorCondition.operation,
+                            identifiers: jDoctorCondition.identifiers,
+                            pre: preConditions,
+                            post: postConditions,
+                            throws: throwsConditions
+                        });
+                    }
+                    folderStructure[`repositories/${repository.projectName}/${repositoryClass.name}.json`] = JSON.stringify({
+                        _id: repositoryClass._id,
+                        name: repositoryClass.name,
+                        jDoctorConditions
+                    }, null, 4);
+                }
+            }
+
+            for (const filePath in folderStructure) {
+                zip.file(filePath, folderStructure[filePath]);
+            }
+
+            const zipStream = zip.generateNodeStream({ type: 'nodebuffer', streamFiles: true });
+
+            res.setHeader('Content-Type', 'application/zip');
+            res.setHeader('Content-Disposition', 'attachment; filename=db.zip');
+
+            zipStream.pipe(res);
+        } catch (e) {
+            // Handle errors here
+            console.error(e);
+            res.status(500).json({ error: e.message });
+        }
+    }
+);
+
+router.get(
+    '/:idUser',
+    getUser,
+    async (req, res) => {
+        try {
+            const zip = new JSZip();
+            const user = res.user;
+            const repositories = await Repository.find({ _id: { $in: user.repositories } });
             const folderStructure = {
                 "inputProjects.json": JSON.stringify(repositories.map(r => { return {
                     projectName: r.projectName,
